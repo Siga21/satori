c3ecb6a9e57254ff6288b205e6500562daf51952
jroca72 <jroca72@gmail.com>
2016-04-20 15:45:14 +0200

    arreglado el error de inicio, tiempo al tiempo

--- satori/cafe/views.py
+++ satori/cafe/views.py
@@ -4,15 +4,14 @@ from datetime import datetime, timedelta
 from django.utils import timezone
 from cafe.models import zonas, locales, tipos_tareas, tareas
 # Create your views here.
-
-class ListaTareas(ListView):
+def index(request):
     initime = datetime.strptime('0000','%H%M').time()
     fintime = datetime.strptime('2359','%H%M').time()
     principio = datetime.combine(datetime.today(), initime)
     fin = datetime.combine(datetime.today(), fintime)
-    queryset = tareas.objects.filter(fecha__gte = principio, fecha__lte = fin, completada = False).order_by('fecha')
-    context_object_name = 'tareas'
-    paginate_by = 8
+    las_tareas = tareas.objects.filter(fecha__gte = principio, fecha__lte = fin, completada = False).order_by('fecha')	
+    context = {'las_tareas': las_tareas }
+    return render(request, 'cafe/index.html', context)
 
 class ListaLocales(ListView):
     queryset = locales.objects.order_by('local')
@@ -27,7 +26,7 @@ class ListaZonas(ListView):
 	context_object_name = 'zonas'
 	paginate_by = 8
 
-class ListaDeTareas(ListView):
+class ListaTareas(ListView):
 	queryset = tareas.objects.order_by('-fecha')
 	context_object_name = 'tareas'
 	paginated_by = 8
